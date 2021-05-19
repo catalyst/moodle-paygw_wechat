318c2026874cd5256b1a7780c97457cc8de7bfc2
Dan Marsden <dan@danmarsden.com>
2021-05-20 09:27:41 +1200

    WeChat works in hundredths, doesn't use decimals.

--- classes/wechat_helper.php
+++ classes/wechat_helper.php
@@ -113,7 +113,7 @@ class wechat_helper {
         $input = new \WxPayUnifiedOrder();
         $input->SetBody($description);
         $input->SetOut_trade_no(self::get_orderid($order));
-        $input->SetTotal_fee($cost);
+        $input->SetTotal_fee($cost * 100); // WeChat works in hundredths and doesn't use decimals.
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 1200));
         $input->SetNotify_url($CFG->wwwroot.'/payment/gateway/wechat/process.php');
